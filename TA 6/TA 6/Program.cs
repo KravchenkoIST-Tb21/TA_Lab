31abf3e568d35ae6753ea82bbb03122d2da3b147
KravchenkoIST-Tb21 <pasha_re97@mail.ru>
2017-06-07 04:28:45 +0300

    Add files via upload

---
+++ TA 6/TA 6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+
+namespace TA_6
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            
+            StreamReader f = File.OpenText("test.txt");
+            string text = f.ReadToEnd();
+            Console.WriteLine("Исходный текст: ");
+            Console.WriteLine(text);
+            Console.WriteLine("\n");
+            Console.WriteLine(File.ReadAllText("test.txt").Split(' ').Count(s => s.Length <= 4));
+            Console.ReadKey();
+        }
+    }
+}
